b7c1b9658cb00d859a34e44b27b7f60b5723eca5
Chase <Gatyas>
2021-11-28 22:01:52 -0600

    Added User page, About page, Search and Alerts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import User from "./components/users/User";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
-function App() {
+import GithubState from "./components/context/github/GithubState";
+import AlertState from "./components/context/alert/AlertState";
+
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar></Navbar>
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;
---
+++ src/components/layout/Alert.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import AlertContext from "../context/alert/alertContext";
+
+// TODO add close button to clear an alert if desired
+
+const Alert = () => {
+  const alertContext = useContext(AlertContext);
+  const { alert } = alertContext;
+
+  return (
+    alert !== null && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className="fas fa-info-circle"></i> {alert.msg}
+      </div>
+    )
+  );
+};
+
+export default Alert;
